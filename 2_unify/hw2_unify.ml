d79c48c02905d11c9d707c5cec1850c0fee9edc0
romagolchin <gol4in.roman@yandex.ru>
2017-06-25 19:25:43 +0300

    done unification

---
+++ 2_unify/hw2_unify.ml
@@ -0,0 +1,92 @@
+module String_map = Map.Make (String);;
+type algebraic_term = Var of string | Fun of string * (algebraic_term list)
+
+(* type substitution = (string * algebraic_term) list
+type equation = algebraic_term * algebraic_term
+type system_of_equations = equation list 
+ *)
+let rec apply_substitution subst_list alg_term = 
+	let subst_map = List.fold_left (fun map (var, rhs) -> String_map.add var rhs map)
+						String_map.empty subst_list in
+	let rec helper term = 
+		match term with
+		| Var x -> if String_map.mem x subst_map then String_map.find x subst_map else Var x
+		| Fun (f, args) -> Fun (f, List.map helper args) 
+	in helper alg_term;;
+
+let check_solution mb_solution system =
+	List.for_all (fun (lhs, rhs) -> 
+		(apply_substitution mb_solution lhs) = (apply_substitution mb_solution rhs)) system;;
+
+let rec is_finite x rhs =
+	match rhs with
+	| Var y -> x <> y
+	| Fun (_, args) -> List.for_all (is_finite x) args;;
+
+let rec string_of_alg_term term = 
+    match term with
+    | Var x -> x
+    | Fun (f, args) -> if f = "Impl" 
+                            then "(" ^ (string_of_alg_term (List.nth args 0)) ^ ") -> " ^ (string_of_alg_term (List.nth args 1))
+                        else let len = List.length args in
+                        fst (List.fold_left (fun (s, i) a -> 
+                            let str = "(" ^ s ^ (string_of_alg_term a) ^ ")" in
+                            if i <> len - 1 
+                                then (str ^ " ", i + 1)
+                            else (str, i + 1)) ("", 0) args);;
+
+
+let string_of_sys sys = 
+    "{\n" ^ (List.fold_left (fun s (lhs, rhs) -> 
+                        s ^ (string_of_alg_term lhs) ^ " = " ^ (string_of_alg_term rhs) ^ "\n"  ) "" sys) ^ "}";;
+
+
+let rec is_contains (x : string) (t : algebraic_term) : bool =
+	match t with
+	| Var y -> x = y
+	| Fun(_, s) -> List.exists (is_contains x) s;;
+
+
+
+let solve_system system =
+	let sys_q = ref (Queue.create ()) in
+	let push_all l = List.iter (fun eq -> Queue.push eq !sys_q) l in
+	push_all system;
+	let to_solution ready = 
+		(* Printf.printf "Solved\n"; *)
+		Printf.printf "%s\n" (string_of_sys ready);
+		List.fold_left (fun res (lhs, rhs) -> match lhs with
+										| Var x -> (match res with
+											| Some some -> Some ((x, rhs) :: some)
+											| _ -> None)
+										| _ -> None) (Some []) ready in
+	let rec helper ready_number eqs_number = 
+		if ready_number = eqs_number 
+			then let subst = 
+				Queue.fold (fun l x -> x :: l) [] !sys_q in to_solution subst
+		else let (lhs, rhs) = Queue.pop !sys_q in
+			if lhs = rhs 
+				(* delete *)
+				then helper 0 (eqs_number - 1)
+			else match (lhs, rhs) with
+				| (Var x, _) -> 
+					if is_finite x rhs 
+						(* eliminate *)
+						then let sub_q = Queue.create () in
+						Queue.fold (fun () (l, r) -> Queue.push (apply_substitution [(x, rhs)] l, apply_substitution [(x, rhs)] r) sub_q) () !sys_q;
+						sys_q := sub_q;
+						Queue.push (lhs, rhs) !sys_q; 
+						helper (ready_number + 1) eqs_number
+					(* check *)
+					else None
+					(* swap *)
+				| (Fun (_, _), Var _) -> Queue.push (rhs, lhs) !sys_q; helper 0 eqs_number
+				| (Fun (f, args), Fun (f', args')) ->  
+					if f <> f' || List.length args <> List.length args' 
+						(* conflict *)
+						then None
+					(* decompose *)
+					else let new_eqs = List.combine args args' in
+					push_all new_eqs;
+					helper 0 (eqs_number + List.length args - 1) in
+	helper 0 (List.length system);;
---
+++ 2_unify/test_unify.ml
@@ -0,0 +1,122 @@
+open Common
+open Printf
+open Hw1
+open Hw2_unify
+open Hw1_reduction
+
+module String_map = Map.Make (String);;
+
+
+let rec string_of_alg_term term = 
+    match term with
+    | Var x -> x
+    | Fun (f, args) -> 
+            if f = "Impl" 
+                then "(" ^ (string_of_alg_term (List.nth args 0)) ^ ") -> " ^ (string_of_alg_term (List.nth args 1))
+            else let len = List.length args in
+            f ^ " " ^ (fst (List.fold_left (fun (s, i) a -> 
+                                let str = "(" ^ s ^ (string_of_alg_term a) ^ ")" in
+                                if i <> len - 1 
+                                    then (str ^ " ", i + 1)
+                                else (str, i + 1)) ("", 0) args));;
+
+
+let string_of_sys sys = 
+    "{\n" ^ (List.fold_left (fun s (lhs, rhs) -> 
+                        s ^ (string_of_alg_term lhs) ^ " = " ^ (string_of_alg_term rhs) ^ "\n"  ) "" sys) ^ "}";;
+
+
+let rename_lambda term =
+    let fv = free_vars term in
+    let base' = List.fold_left (fun longest_var var -> 
+                                    if String.length longest_var < String.length var 
+                                        then var
+                                        else longest_var) "" fv in 
+    let base = if String.length base' = 0 then "x" else base' in
+    let cnt = ref 0 in 
+    let rec helper t bound = 
+        match t with
+        | Common.Var x -> let x' = if String_map.mem x bound then String_map.find x bound else x
+                                in Common.Var x'
+        | App (p, q) -> App (helper p bound, helper q bound)
+        | Abs (var, body) -> cnt := !cnt + 1;
+                             let var' = (base ^ (string_of_int !cnt)) in
+                            let bound' = String_map.add var var' bound in
+            Abs (var', helper body bound') in
+    helper term String_map.empty;;
+
+let lambda_to_system (term : lambda) : ((algebraic_term * algebraic_term) list) * algebraic_term =
+    let cnt = ref 0 in
+    let impl l r = Fun ("Impl", [l; r]) in
+    let rec helper t =
+        match t with
+        | Common.Var x -> 
+            ([], Hw2_unify.Var ("t" ^ x))
+        | App (p, q) -> cnt := !cnt + 1; 
+                        let new_type = Hw2_unify.Var ("t" ^ (string_of_int !cnt)) in
+                        let (sys_p, type_p) = helper p in
+                        let (sys_q, type_q) = helper q in
+                        ((type_p, impl type_q new_type) 
+                            :: (List.rev_append  sys_p sys_q), new_type)
+        | Abs (x, body) -> 
+                        let res = helper body in
+                        (fst res, impl (Var ("t" ^ x)) (snd res))
+    in let (sys, tp) = helper term in
+    (sys, tp) ;;
+
+
+let rec print_substitution sub =
+    match sub with 
+    | [] -> print_string "\n"
+    | (h::t) -> print_string ((fst h) ^ " = " ^ (string_of_alg_term (snd h)) ^ "\n"); print_substitution t;; 
+
+let eqt11 = Fun("x", [Var "p1"]), Fun("x", [Var "p2"]) 
+let eqt12 = Fun("y", [Var "p2"]), Fun("y", [Var "p4"])
+let eqt13 = Fun("z", [Var "p5"]), Fun("z", [Var "p6"])
+
+let eqt21 = Fun("x", [Var "p1"]), Fun("x", [Var "p2"]) 
+let eqt22 = Fun("m", [Var "p1"]), Fun("y", [Var "p4"])
+let eqt23 = Fun("z", [Var "p5"]), Fun("z", [Var "p6"])
+
+let eqt31 = Fun("x", [Var "p1"]), Fun("x", [Var "p2"]) 
+let eqt32 = Fun("y", [Var "p1"]), Fun("y", [Var "p4"])
+let eqt33 = Fun("z", [Var "p1"]), Fun("z", [Var "p6"])
+
+let syms = [[eqt11; eqt12; eqt13];
+[eqt21; eqt22; eqt23];
+[eqt31; eqt32; eqt33]];;
+
+let () =
+    (* let w = lambda_of_string "\\x.x x" in *)
+    let i = lambda_of_string "\\x.x" in
+    let k = lambda_of_string "\\x.\\y.x" in
+    let s = lambda_of_string "\\x.\\y.\\z.x z (y z)" in
+    let t = lambda_of_string "\\x.\\y.\\z.x z y" in
+    let fls = lambda_of_string "\\x.\\y.y" in
+    let vara = Common.Var "a" in
+    let varb = Common.Var "b" in
+    let neg = Abs ("a", App (App (vara, fls), k)) in
+    let xor = Abs ("a", Abs ("b", App (App (vara, App (neg, varb)), varb))) in
+    let test = [i; k; s; t; App (s, k); App (App (s, k), k); App (i, App (i, s)); neg; xor] in
+    (* let (sys, tp) = lambda_to_system (Abs ("x", Common.Var "y")) in *)
+    printf "Oktai's \n";
+    List.iter (fun sys ->   printf "System:\n%s\n" (string_of_sys sys);
+                            let sol = solve_system sys
+                            in match sol with
+                            | None -> printf "No solution\n"
+                            | Some x -> (* print_substitution x; *)
+                                (check_solution x sys) |> printf "%B\n") syms;
+
+    printf "Mine \n";
+    List.iter (fun comb -> 
+                printf "Term: %s\n" (string_of_lambda comb);
+                printf "Nice: %s\n" (comb |> rename_lambda |> string_of_lambda);
+                let (sys, tp) = comb |> rename_lambda |> lambda_to_system in
+                let solution = match (solve_system sys) with
+                | Some x -> x
+                | _ -> failwith "No solution, no type" in
+                let subst_type = apply_substitution solution tp in
+                printf "%s\n" (string_of_alg_term subst_type);
+                printf "%B\n" (check_solution solution sys);
+                (* print_substitution solution; *)
+            ) test;;
